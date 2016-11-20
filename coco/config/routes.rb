77237378a38edb2dcb640fa1b76447c87c9dc8b5
Jasmine Lu <jll68@duke.edu>
2016-11-20 05:45:44 +0000

    website added

--- coco/config/routes.rb
+++ coco/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/homepage'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#homepage'
 end
